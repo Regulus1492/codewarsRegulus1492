2fc01057c6609912dda87bf562f268d825712b9e
Regulus1492 <epsilona.70@gmail.com>
2024-03-17 11:08:07 -0500

    who likes it? kata

---
+++ wholikesit.py
@@ -0,0 +1,68 @@
+
+# You probably know the "like" system from Facebook and other pages.
+# People can "like" blog posts, pictures or other items. 
+# We want to create the text that should be displayed next to such an item.
+
+# Implement the function which takes an array containing 
+# the names of people that like an item. It must return the 
+# display text as shown in the examples:
+
+# []                                -->  "no one likes this"
+# ["Peter"]                         -->  "Peter likes this"
+# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+# Other solutions
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
+
+def likes(names):
+    # make a dictionary d of all the possible answers. Keys are the respective number
+    # of people who liked it.
+    
+    # {} indicate placeholders. They do not need any numbers but are simply 
+    # replaced/formatted
+    # in the order the arguments in names are given to format
+    # {others} can be replaced by its name; below the argument "others = length - 2"
+    # is passed to str.format()
+    d = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+        }
+    length = len(names)
+    # d[min(4, length)] insures that the appropriate string is called from the dictionary
+    # and subsequently returned. Min is necessary as len(names) may be > 4
+    
+    # The * in *names ensures that the list names is blown up and that format is called
+    # as if each item in names was passed to format individually, i. e.
+    # format(names[0], names[1], .... , names[n], others = length - 2
+    return d[min(4, length)].format(*names, others = length - 2)
+
+
+# my solution
+
+def likes(names):
+    if not names:
+        text_print = "no one likes this"
+    else:
+        if len(names) == 1:
+            text_print = f"{names[0]} likes this"
+        elif len(names) == 2:
+            text_print = " and ".join(names) + " like this"
+        elif len(names) == 3:
+            text_print = f"{names[0]}, {names[1]} and {names[2]} like this"
+        else:
+            text_print = f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+        
+    return text_print
